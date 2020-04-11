fd78e2d7eb4ec1be0346bbec4ccbcadd75658b63
openyogaprojects <55026715+openyogaprojects@users.noreply.github.com>
2020-04-11 18:07:54 +0300

    Update issue_patch.rb

--- lib/redmine_auto_percent/issue_patch.rb
+++ lib/redmine_auto_percent/issue_patch.rb
@@ -7,7 +7,6 @@ module RedmineAutoPercent
 
       base.class_eval do
         before_save :custom_field_set_value
- 		before_save :update_due_date
       end
     end
   end
@@ -16,47 +15,52 @@ module RedmineAutoPercent
   end
 
   module InstanceMethods
-	  
-class BitcoinRPC
-  def initialize(service_url)
-    @uri = URI.parse(service_url)
-  end
- 
-  def method_missing(name, *args)
-    post_body = { 'method' => name, 'params' => args, 'id' => 'jsonrpc' }.to_json
-    resp = JSON.parse( http_post_request(post_body) )
-    raise JSONRPCError, resp['error'] if resp['error']
-    resp['result']
-  end
- 
-  def http_post_request(post_body)
-    http    = Net::HTTP.new(@uri.host, @uri.port)
-    request = Net::HTTP::Post.new(@uri.request_uri)
-    request.basic_auth @uri.user, @uri.password
-    request.content_type = 'application/json'
-    request.body = post_body
-    http.request(request).body
-  end
- 
-  class JSONRPCError < RuntimeError; end
-end
-	 
-def custom_field_set_value
-  self.custom_field_values.each do |field|
-	   if ( ( field.custom_field.id == 3 ) && self.status.is_closed?)
-      @address = field.value
+    class BitcoinRPC
+      def initialize(service_url)
+        @uri = URI.parse(service_url)
+      end
+
+      def method_missing(name, *args)
+        post_body = { 'method' => name, 'params' => args, 'id' => 'jsonrpc' }.to_json
+        resp = JSON.parse( http_post_request(post_body) )
+        raise JSONRPCError, resp['error'] if resp['error']
+        resp['result']
+      end
+
+      def http_post_request(post_body)
+        http    = Net::HTTP.new(@uri.host, @uri.port)
+        request = Net::HTTP::Post.new(@uri.request_uri)
+        request.basic_auth @uri.user, @uri.password
+        request.content_type = 'application/json'
+        request.body = post_body
+        http.request(request).body
+      end
+
+      class JSONRPCError < RuntimeError
+      end
+
     end
-    if ( ( field.custom_field.id == 2 ) && self.status.is_closed?)
-      h = BitcoinRPC.new('http://user:password@127.0.0.1:8332')
-      @amount = field.value
-      @tx = h.sendtoaddress(@address, @amount)
+
+    def custom_field_set_value
+      if !self.assigned_to.nil? && !self.done_ratio.zero? && self.status.is_closed?
+        tx = self.custom_field_value(4)
+        self.custom_field_values.each do |field|
+          if  ( field.custom_field.id == 2 )
+            h = BitcoinRPC.new('http://user:pass@host:21710')
+            address = self.assigned_to.custom_field_value(3)
+            maxsalary = field.value.to_f
+            ln_from_maxsalary = Math.log(maxsalary)
+            salary = Math.exp( ln_from_maxsalary / 100 * self.done_ratio )
+              if !maxsalary.zero? && !address.blank? && tx.blank?
+                account = "redmine"
+                tx = h.sendfrom(account, address, salary)
+              end
+          end
+          if ( field.custom_field.id == 4 )
+            field.value = tx
+          end
+        end
+      end
     end
-  end
-end
- 	def update_due_date
- 	  if (!(self.done_ratio == self.status.default_done_ratio) && self.status.is_closed?)
-		self.due_date = Date.today
-	  end
-	end
   end
 end
